a06ead19fa8d1a1d9ef9ddfc6a8131ed4a534d96
Nick Dumas <drakonik@gmail.com>
2018-07-18 18:59:29 +0000

    Determined a scaling factor that produces correct quanization.
    The catch is that it requires the output domain be balanced around zero,
    e.g. [-5,5], [-250, 250].
    
    This scaling would not work for [-750, 500] or [-7000, -2000]. It's a
    good start, though. Python unit tests are passing.

--- q.py
+++ q.py
@@ -1,19 +1,36 @@
 import math
 
-# sign = lambda x: (1, -1)[x < 0]
+def mu(x, steps):
+    sign = lambda x: (1, -1)[x < 0]
+    return sign(x) * math.log(1 + (len(steps)*abs(x))) / math.log(1 + len(steps))
+
+def abs_diff(x, y):
+    return abs(min(x, y) - max(x,y))
+
+def scaleSteps(steps):
+    pass
 
 def quantize(steps, x):
     distances = []
     stepIndex = None
 
-    # scale = (len(steps)/(len(steps)-1))
+    # scale = abs_diff(min(steps), max(steps)) / len(steps)
+
+    if abs(min(steps)) == abs(max(steps)):
+        scale = max(steps)
+    else:
+        scale = max(scaleSteps(steps))
+    scaled = x * scale
 
     for step in steps:
-        # distances.append(step - x)
-        distances.append(abs(step) - abs(x))
+        distances.append(abs_diff(scaled, step))
+
+    # distances = [abs_diff(scaled, step) for step in steps]
 
     minDistance = min(distances)
-    print("Value:{0}\nDistances:{1}\nMinDistance:{2}".format(x, distances, minDistance))
+    # print("Scaling factor: {0}".format(scale))
+    # print("Scaled `x`: {0}".format(scaled))
+    # print("Value:{0}\nDistances:{1}\nMinDistance:{2}\n=====".format(x, distances, minDistance))
 
     return steps[distances.index(minDistance)]
 
@@ -26,3 +43,7 @@ output_domain = list(range(-5, 6, 1))
 # This is an example input set, specifically crafted to match the output domain above.
 # -1 should be mapped to -5, -0.8 should map to -4, etc.
 fs = [-1, -.8, -.6, -.4, -.2, 0, .2, .4, .6, .8, 1]
+
+if __name__ == '__main__':
+    o = Q(output_domain, fs)
+    print(o)
