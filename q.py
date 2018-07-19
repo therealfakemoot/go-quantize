8b725561fe21230733cca7844cc2a60022ce3040
Nick Dumas <drakonik@gmail.com>
2018-07-19 13:53:03 +0000

    Remove extraneous code.
    No more mu func, that one's a bust. Maybe it only works for purely
    positive output domains. Oh well.
    
    Removed a bunch of debug prints and condensed the code down to keep the
    functions short enough to read in one screen.

--- q.py
+++ q.py
@@ -1,38 +1,24 @@
 import math
 
-def mu(x, steps):
-    sign = lambda x: (1, -1)[x < 0]
-    return sign(x) * math.log(1 + (len(steps)*abs(x))) / math.log(1 + len(steps))
-
 def abs_diff(x, y):
     return abs(min(x, y) - max(x,y))
 
 def scaleSteps(steps):
-    pass
+    raise NotImplementedError
 
 def quantize(steps, x):
-    distances = []
     stepIndex = None
-
-    # scale = abs_diff(min(steps), max(steps)) / len(steps)
 
     if abs(min(steps)) == abs(max(steps)):
         scale = max(steps)
     else:
         scale = max(scaleSteps(steps))
+
     scaled = x * scale
 
-    for step in steps:
-        distances.append(abs_diff(scaled, step))
+    distances = [abs_diff(scaled, step) for step in steps]
 
-    # distances = [abs_diff(scaled, step) for step in steps]
-
-    minDistance = min(distances)
-    # print("Scaling factor: {0}".format(scale))
-    # print("Scaled `x`: {0}".format(scaled))
-    # print("Value:{0}\nDistances:{1}\nMinDistance:{2}\n=====".format(x, distances, minDistance))
-
-    return steps[distances.index(minDistance)]
+    return steps[distances.index(min(distances))]
 
 def Q(steps, fs, func=quantize):
     return [func(steps, f) for f in fs]
