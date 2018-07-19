6ce66e123b83f1b7cefa0b1fec0a75edc6053b60
Nick Dumas <drakonik@gmail.com>
2018-07-19 14:27:23 +0000

    Tweak tests to work with new yielding Q() func.

--- test_q.py
+++ test_q.py
@@ -11,12 +11,11 @@ class TestBalancedDomain(unittest.TestCase):
 
         expected = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
         actual = Q(steps, fs)
-        self.assertEqual(expected, actual)
+        self.assertEqual(expected, list(actual))
 
     def testSingleValue(self):
         steps = range(-5, 6, 1)
         steps = list(steps)
-
 
         expected = -5
         actual = quantize(steps, -1)
@@ -31,12 +30,11 @@ class TestUnbalancedDomain(unittest.TestCase):
 
         expected = [-10, -7, -4, -1, 2, 5, 8, 11, 14, 17, 20]
         actual = Q(steps, fs)
-        self.assertEqual(expected, actual)
+        self.assertEqual(expected, list(actual))
 
     def testSingleValue(self):
         steps = range(-10, 21, 3)
         steps = list(steps)
-
 
         expected = -10
         actual = quantize(steps, -1)
