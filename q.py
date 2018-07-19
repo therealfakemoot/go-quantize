71bbbe01023a6c8af300ab095cffb53ff5fda259
Nick Dumas <drakonik@gmail.com>
2018-07-19 14:29:44 +0000

    Minor reorganization.
    Moving this stuff into the __main__ block for clarity/consistency.

--- q.py
+++ q.py
@@ -22,13 +22,14 @@ def quantize(steps, x):
 
     return steps[distances.index(min(distances))]
 
-# The output domain is the set of possible values that inputs can be mapped to.
-output_domain = list(range(-5, 6, 1))
-
-# This is an example input set, specifically crafted to match the output domain above.
-# -1 should be mapped to -5, -0.8 should map to -4, etc.
-fs = [-1, -.8, -.6, -.4, -.2, 0, .2, .4, .6, .8, 1]
-
 if __name__ == '__main__':
+
+    # The output domain is the set of possible values that inputs can be mapped to.
+    output_domain = list(range(-5, 6, 1))
+
+    # This is an example input set, specifically crafted to match the output domain above.
+    # -1 should be mapped to -5, -0.8 should map to -4, etc.
+    fs = [-1, -.8, -.6, -.4, -.2, 0, .2, .4, .6, .8, 1]
+
     o = Q(output_domain, fs)
     print(o)
