80f87a20a1a1508770aa5722e344526f1c870049
Nick Dumas <drakonik@gmail.com>
2018-07-21 14:32:24 +0000

    Implement Balance-and-Shift quantization. All tests passing.
    Transliteration from Python implementation. Balance-and-Shift takes
    advantage of the fact that input domain is [-1,1], meaning arbitrary
    scaling of the input space to match any output domain trivial.
    
    Once the inputs are scaled, the output domain gets shifted to be
    'balanced' around 0, such that min(domain) == max(domain). This matches
    the inputs and outputs together. The smallest possible input vaue is `-1
    * min(domain)` and the largest is `(1 * max(domain)`. A little bit of
    absolute difference math helps find the nearest output domain value to
    each input.

--- domain.go
+++ domain.go
@@ -2,6 +2,33 @@ package quantize
 
 import "math"
 
+func absDiff(x, y float64) float64 {
+	return math.Abs(math.Min(x, y) - math.Max(x, y))
+}
+
+func midpoint(steps []float64) float64 {
+	return (steps[0] + steps[len(steps)-1]) / 2
+}
+
+func balanceSteps(steps []float64) []float64 {
+	var shifted []float64
+	var shifter func(float64, float64) float64
+	m := midpoint(steps)
+	shift := absDiff(m, 0)
+
+	if math.Abs(Min(steps)) < math.Abs(Max(steps)) {
+		shifter = func(s, x float64) float64 { return x - s }
+	} else {
+		shifter = func(s, x float64) float64 { return x + s }
+	}
+
+	for _, s := range steps {
+		shifted = append(shifted, shifter(s, shift))
+	}
+
+	return shifted
+}
+
 // Domain describes the integer space to which float values must be mapped.
 type Domain struct {
 	Min  float64
@@ -23,30 +50,32 @@ func (d Domain) Steps() []float64 {
 // Quantize normalizes a given set of arbitrary inputs into the provided output Domain.
 func (d Domain) Quantize(fs []float64) []float64 {
 	var ret []float64
+	var balanced []float64
+	var scale float64
 
 	steps := d.Steps()
 
-	quantize := func(x float64) float64 {
-		var distances []float64
-		var stepIndex int
-		for _, step := range steps {
-			distances = append(distances, math.Abs(math.Abs(x)-math.Abs(step)))
-		}
-		minDistance := Min(distances)
-
-		for i, d := range distances {
-			if d == minDistance {
-				stepIndex = i
-				break
-			}
-		}
-		return steps[stepIndex]
+	if math.Abs(Min(steps)) == math.Abs(Max(steps)) {
+		balanced = steps
+		scale = Max(steps)
+	} else {
+		balanced = balanceSteps(steps)
+		scale = Max(balanced)
 	}
 
-	// quantaSize := (d.Max - d.Min) / (math.Pow(2.0, stepFloat) - 1.0)
+	for _, val := range fs {
+		var distances []float64
+		scaled := val * scale
+		for _, step := range balanced {
+			distances = append(distances, absDiff(scaled, step))
+		}
+		minDist := Min(distances)
 
-	for _, f := range fs {
-		ret = append(ret, quantize(f))
+		for i, dist := range distances {
+			if minDist == dist {
+				ret = append(ret, steps[i])
+			}
+		}
 	}
 
 	return ret
