a344792686e20aac2a12fe7d74303516cbcb2ccb
Nick Dumas <drakonik@gmail.com>
2018-07-21 13:50:09 +0000

    Complete, ideally correct quantization implementation. All tests passing.
    This one got pretty tricky but I finally got the inspiration I needed to
    solve it. Previous implementation was only able to quantize 'balanced'
    domains e.g. [-20, 20]. It failed for domains like [-25, 10]. This was
    because the 'distance' algorithm couldn't compensate for the
    'unbalanced' nature of the domains.
    
    An if/else in quantize and the addition of balanceSteps creates a
    'shifted' version of the steps array which is used for distance
    calculations. This works because the shifted array's indices still
    correlate to the original steps.

--- q.py
+++ q.py
@@ -3,24 +3,37 @@ import math
 def abs_diff(x, y):
     return abs(min(x, y) - max(x,y))
 
-def scaleSteps(steps):
-    raise NotImplementedError
+midpoint = lambda x: (x[0] + x[-1])/2.0
+
+def balanceSteps(steps):
+    m = midpoint(steps)
+    shift = abs_diff(m, 0)
+
+    if abs(min(steps)) < abs(max(steps)):
+        shifted = [step - shift for step in steps]
+    else:
+        shifted = [step + shift for step in steps]
+
+    return shifted
 
 def Q(steps, fs):
-    for f in fs:
-        yield(quantize(steps, f))
-
-def quantize(steps, x):
     if abs(min(steps)) == abs(max(steps)):
+        balanced = steps
         scale = max(steps)
     else:
-        scale = max(scaleSteps(steps))
+        balanced = balanceSteps(steps)
+        scale = max(balanced)
 
-    scaled = x * scale
+    def quantize(x):
+        scaled = x * scale
 
-    distances = [abs_diff(scaled, step) for step in steps]
+        distances = [abs_diff(scaled, step) for step in balanced]
 
-    return steps[distances.index(min(distances))]
+        return steps[distances.index(min(distances))]
+
+    for f in fs:
+        yield quantize(f)
+
 
 if __name__ == '__main__':
 
@@ -31,5 +44,5 @@ if __name__ == '__main__':
     # -1 should be mapped to -5, -0.8 should map to -4, etc.
     fs = [-1, -.8, -.6, -.4, -.2, 0, .2, .4, .6, .8, 1]
 
-    o = Q(output_domain, fs)
-    print(o)
+    # o = Q(output_domain, fs)
+    # print(o)
