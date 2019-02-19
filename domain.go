cabf9e14e235d0e4260f1ef01fc5cf028e0c5442
Nick Dumas <nicholas.dumas@homes.com>
2019-02-19 15:26:02 +0000

    Fundamental rework of quantization.
    Thanks to Sgt_Tailor in discord for dropping this knowledge bomb on me.
    Next step is to work on the tests and see if it really holds up under
    fire.

--- domain.go
+++ domain.go
@@ -1,6 +1,8 @@
 package quantize
 
-import "math"
+import (
+	"fmt"
+)
 
 // Domain describes the integer space to which float values must be mapped.
 type Domain struct {
@@ -22,61 +24,11 @@ func (d Domain) Steps() []float64 {
 
 // Quantize normalizes a given set of arbitrary inputs into the provided output Domain.
 func (d Domain) Quantize(fs []float64) []float64 {
-	var ret []float64
-	var balanced []float64
-	var scale float64
+	fmt.Println("Entering Quantize().")
+	var ret = make([]float64, len(fs))
 
-	steps := d.Steps()
-
-	if math.Abs(Min(steps)) == math.Abs(Max(steps)) {
-		balanced = steps
-		scale = Max(steps)
-	} else {
-		balanced = balanceSteps(steps)
-		scale = Max(balanced)
+	for idx, f := range fs {
+		ret[idx] = (f-(-1))*(d.Max-d.Min)/(1-(-1)) + d.Min
 	}
-
-	for _, val := range fs {
-		var distances []float64
-		scaled := val * scale
-		for _, step := range balanced {
-			distances = append(distances, absDiff(scaled, step))
-		}
-		minDist := Min(distances)
-
-		for i, dist := range distances {
-			if minDist == dist {
-				ret = append(ret, steps[i])
-			}
-		}
-	}
-
 	return ret
-}
-
-func absDiff(x, y float64) float64 {
-	return math.Abs(math.Min(x, y) - math.Max(x, y))
-}
-
-func midpoint(steps []float64) float64 {
-	return (steps[0] + steps[len(steps)-1]) / 2
-}
-
-func balanceSteps(steps []float64) []float64 {
-	var shifted []float64
-	var shifter func(float64, float64) float64
-	m := midpoint(steps)
-	shift := absDiff(m, 0)
-
-	if math.Abs(Min(steps)) < math.Abs(Max(steps)) {
-		shifter = func(s, x float64) float64 { return x - s }
-	} else {
-		shifter = func(s, x float64) float64 { return x + s }
-	}
-
-	for _, s := range steps {
-		shifted = append(shifted, shifter(s, shift))
-	}
-
-	return shifted
 }
