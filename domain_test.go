626acd0b7d297b81def88b298bee59349d7859a1
Nick Dumas <nicholas.dumas@homes.com>
2019-02-25 19:11:46 +0000

    Removing useless tests.

--- domain_test.go
+++ domain_test.go
@@ -1,7 +1,6 @@
 package quantize_test
 
 import (
-	"fmt"
 	Q "github.com/therealfakemoot/go-quantize"
 	"reflect"
 	"testing"
@@ -22,46 +21,6 @@ func buildTestDomains(edge float64) []Q.Domain {
 		ret = append(ret, Q.Domain{Min: x, Max: -x})
 	}
 	return ret
-}
-
-func assertDomainMin(d Q.Domain, fs []float64) bool {
-
-	min := Q.Min(fs)
-	return !(min < d.Min)
-}
-
-func assertDomainMax(d Q.Domain, fs []float64) bool {
-
-	max := Q.Max(fs)
-	return !(max > d.Max)
-}
-
-func TestDomainBounds(t *testing.T) {
-	domains := buildTestDomains(edge)
-
-	for _, d := range domains {
-		t.Run(fmt.Sprintf("Domain:[%0.f,%0.f]", d.Min, d.Max), func(t *testing.T) {
-			for i := mapMin; i <= mapMax; i += mapStep {
-				fs := Q.Noise(i, i, seed)
-				quantized := d.Quantize(fs)
-				t.Run(fmt.Sprintf("Map(%0.f,%0.f:%d)", i, i, seed), func(t *testing.T) {
-					t.Run("Min", func(t *testing.T) {
-						if !assertDomainMin(d, quantized) {
-							t.Fail()
-						}
-					})
-
-					t.Run("Max", func(t *testing.T) {
-						if !assertDomainMax(d, quantized) {
-							t.Fail()
-						}
-					})
-
-				})
-			}
-		})
-	}
-
 }
 
 func TestKnownValues(t *testing.T) {
