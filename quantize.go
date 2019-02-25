54516c827ffcf1405e78d2cb5ddfe6cb5dbd5e04
Nick Dumas <nicholas.dumas@homes.com>
2019-02-25 14:43:28 +0000

    this isn't useful anymore because i have unit tests

--- quantize.go
+++ quantize.go
@@ -1,21 +1,6 @@
 package quantize
 
-import (
-	"fmt"
-)
+import ()
 
 func main() {
-	d := Domain{
-		Min:  -5.0,
-		Max:  5.0,
-		Step: 1.0,
-	}
-
-	fs := Noise(5, 5, 8675309)
-
-	v := d.Quantize(fs)
-
-	fmt.Printf("Output Domain: %+v\n", d)
-	fmt.Printf("Input: %v\n", fs)
-	fmt.Printf("Output: %v\n", v)
 }
